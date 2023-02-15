8a672a97bea74e181cc75fc1dfa1650837bfa20b
Mohammed Mubashir <mohammedmubashir204@gmail.com>
2023-02-15 15:19:48 +0530

    - add a spin button - add logic to randomly change images - keep track of the chances used to spin - keep track how many times won - bug fixes

--- Slot Machine/ContentView.swift
+++ Slot Machine/ContentView.swift
@@ -39,15 +39,17 @@ enum Choice : Int, Identifiable {
         rawValue
     }
     
-    case Success, failure
+    case success, failure
     
 }
 
 struct ContentView: View {
     
-    @State public var symbols = ["eating","happy","love"]
-    @State public var numbers = [0,1,2]
+    @State public var symbols = ["eating","happy","love","scary","sleeping"]
+    @State public var numbers = [0,1,2,3,4]
+    @State public var attempts = 8
     @State public var counter = 0
+    @State public var wins = 0
     @State private var showingAlert:Choice?
     
     
@@ -56,7 +58,7 @@ struct ContentView: View {
         ZStack {
             Image("sunshine").resizable().ignoresSafeArea()
             
-            VStack(alignment: .center, spacing: 80) {
+            VStack(alignment: .center, spacing: 50) {
                 HStack {
                     Image("fire")
                         .resizable()
@@ -74,14 +76,15 @@ struct ContentView: View {
                         .shadow(color: .orange, radius: 1,y: 3)
                 }
                 .frame(width: .infinity,height: 50,alignment: .center)
-                
+   
+                Text("\(attempts) out of 8 attempts left...").font(.system(size: 20)).shadow(color: .orange, radius: 1,y: 3)
                 
                 VStack(spacing: 15) {
                     HStack(spacing: 35) {
                         
                         Hexagona().fill(Color.white.opacity(0.8)).frame(width: 100,height: 120,alignment: .center)
                             .overlay(
-                                Image(symbols[0])
+                                Image(symbols[numbers[0]])
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: 80,height: 70,alignment: .center)
@@ -91,32 +94,25 @@ struct ContentView: View {
                         
                         Hexagona().fill(Color.white.opacity(0.8)).frame(width: 100,height: 120,alignment: .center)
                             .overlay(
-                                Image(symbols[0])
+                                Image(symbols[numbers[1]])
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: 80,height: 70,alignment: .center)
                                     .shadow(color: .gray,radius: 4,x: 4,y: 5)
                             )
                     }
-                    
-                    
                     Hexagona().fill(Color.white.opacity(0.8)).frame(width: 100,height: 120,alignment: .center)
                         .overlay(
-                            Image(symbols[0])
+                            Image(symbols[numbers[2]])
                                 .resizable()
                                 .scaledToFit()
                                 .frame(width: 80,height: 70,alignment: .center)
-                                .shadow(color: .gray,radius: 4,x: 4,y: 5)
-                        )
-                    
-                    
-                    
-                    
+                                .shadow(color: .gray,radius: 4,x: 4,y: 5))
                     HStack(spacing: 35) {
                         
                         Hexagona().fill(Color.white.opacity(0.8)).frame(width: 100,height: 120,alignment: .center)
                             .overlay(
-                                Image(symbols[0])
+                                Image(symbols[numbers[3]])
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: 80,height: 70,alignment: .center)
@@ -126,13 +122,70 @@ struct ContentView: View {
                         
                         Hexagona().fill(Color.white.opacity(0.8)).frame(width: 100,height: 120,alignment: .center)
                             .overlay(
-                                Image(symbols[0])
+                                Image(symbols[numbers[4]])
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: 80,height: 70,alignment: .center)
                                     .shadow(color: .gray,radius: 4,x: 4,y: 5)
                             )
                     }
+                    
+                    Text("\(wins) wins").font(.system(size: 20)).shadow(color: .orange, radius: 1,y: 3)
+                }
+                
+                Button {
+
+                    numbers[0] = Int.random(in: 0...numbers.count-1)
+                    numbers[1] = Int.random(in: 0...numbers.count-1)
+                    numbers[2] = Int.random(in: 0...numbers.count-1)
+                    numbers[3] = Int.random(in: 0...numbers.count-1)
+                    numbers[4] = Int.random(in: 0...numbers.count-1)
+                    
+                    counter += 1
+                    attempts -= 1
+                    
+                    
+                    if numbers[0] == numbers[1] && numbers[1] == numbers[2] && numbers[2] == numbers[3] && numbers[3] == numbers[4]
+                    {
+                        self.showingAlert = .success
+                        counter = 0
+                        attempts = 8
+                        wins += 1
+                    }
+                    
+                    
+                    if counter > 7 {
+                        self.showingAlert = .failure
+                        counter = 0
+                        attempts = 8
+                        
+                    }
+       
+                } label: {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color("color"))
+                        .overlay(
+                            Text("Spin")
+                                .fontWeight(.bold)
+                                .font(.title3)
+                            
+                        )
+            
+                        .foregroundColor(.black)
+                        .frame(width: 200,height: 40,alignment: .center)
+                        .shadow(color: .gray, radius: 1,y: 4)
+                }
+
+            }
+            .alert(item: $showingAlert) { alert -> Alert in
+                
+                switch alert {
+                case .success:
+                    return Alert(title: Text("Yahhh! You won"),message: Text("Born with the charm"),dismissButton: .cancel())
+                    
+                case .failure:
+                    return Alert(title: Text("Oooooops"),message: Text("No worries, You can always try again"),dismissButton: .cancel())
+                    
                 }
             }
         }
